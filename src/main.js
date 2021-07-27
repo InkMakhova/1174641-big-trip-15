3919f2bafe403f91473a42fbed001dbc90b91d7f
Инна <i.m.makhova@gmail.com>
2021-07-28 00:13:50 +0300

    Добавлены компоненты информация о путешествии, фильтры

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import {createSiteMenuTemplate} from './view/site-menu.js';
+import {createTripInfoTemplate} from './view/trip-info.js';
+import {createFilterTemplate} from './view/filter.js';
+
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
+
+const siteHeaderElement = document.querySelector('.page-header');
+const tripMainElement = siteHeaderElement.querySelector('.trip-main');
+const siteMenuElement = siteHeaderElement.querySelector('.trip-controls__navigation');
+const filterElement = siteHeaderElement.querySelector('.trip-controls__filters');
+
+render(tripMainElement, createTripInfoTemplate(), 'afterbegin');
+render(siteMenuElement, createSiteMenuTemplate(), 'beforeend');
+render (filterElement, createFilterTemplate(), 'beforeend');
