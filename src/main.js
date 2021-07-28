1efd2843506c82e24bf944babb44a3253de252c6
Инна <i.m.makhova@gmail.com>
2021-07-28 16:12:27 +0300

    Добавлены файлы компонентов: форма нового направления, форма редактирования, цена, точка в списке, сортировка

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import {createSiteMenuTemplate} from './view/site-menu.js';
 import {createTripInfoTemplate} from './view/trip-info.js';
+import {createPriceTemplate} from './view/price.js';
 import {createFilterTemplate} from './view/filter.js';
+import {createSortTemplate} from './view/sort.js';
+import {createPointListTemplate} from './view/point-list';
+import {createEditPointTemplate} from './view/edit-point-form.js';
+import {createNewPointTemplate} from './view/new-point-form';
+import {createPointTemplate} from './view/point.js';
+
+const POINTS = 3;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -14,3 +22,23 @@ const filterElement = siteHeaderElement.querySelector('.trip-controls__filters')
 render(tripMainElement, createTripInfoTemplate(), 'afterbegin');
 render(siteMenuElement, createSiteMenuTemplate(), 'beforeend');
 render (filterElement, createFilterTemplate(), 'beforeend');
+
+const tripInfoElement = tripMainElement.querySelector('.trip-info');
+
+render(tripInfoElement, createPriceTemplate(), 'beforeend');
+
+const siteMainElement = document.querySelector('.page-main');
+const tripEventsElement = siteMainElement.querySelector('.trip-events');
+
+render(tripEventsElement, createSortTemplate(), 'beforeend');
+render(tripEventsElement, createPointListTemplate(), 'beforeend');
+
+const tripEnventsListElement = tripEventsElement.querySelector('.trip-events__list');
+
+render(tripEnventsListElement, createEditPointTemplate(), 'beforeend');
+render(tripEnventsListElement, createNewPointTemplate(), 'beforeend');
+
+for (let i = 0; i < POINTS; i++) {
+  render(tripEnventsListElement, createPointTemplate(), 'beforeend');
+}
+
