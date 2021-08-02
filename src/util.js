db040fea10f030ab667e71d6b08c99cbaf5d7b40
Инна <i.m.makhova@gmail.com>
2021-08-02 14:47:41 +0300

    Добавлено форматирование даты для разметки

--- src/util.js
+++ src/util.js
@@ -7,6 +7,6 @@ export const getRandomInteger = (a = 0, b = 1) => {
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
-export const formatDatePointList = (date) => dayjs(date).format('MMM D');
-
+export const formatDatePointList = (date) => dayjs(date).format('YYYY-MM-DD');
+export const humanizeDatePointList = (date) => dayjs(date).format('MMM D');
 export const formatDateTimePointForm = (dateTime) => dayjs(dateTime).format('DD/MM/YY hh:mm');
