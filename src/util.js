58b6b622b3a673c900d5bc7ef98b2a63286c00eb
Инна <i.m.makhova@gmail.com>
2021-08-02 14:22:53 +0300

    Добавлены функции форматирования даты и времени

--- src/util.js
+++ src/util.js
@@ -1,6 +1,12 @@
+import dayjs from 'dayjs';
+
 export const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
 
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
+
+export const formatDatePointList = (date) => dayjs(date).format('MMM D');
+
+export const formatDateTimePointForm = (dateTime) => dayjs(dateTime).format('DD/MM/YY hh:mm');
