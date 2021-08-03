d694841c1078582c7f83bd3a9dda0b3de46daab5
Инна <i.m.makhova@gmail.com>
2021-08-03 14:04:24 +0300

    В константы добавлены форматы даты и времени

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const formatsDateTime = {
+  yearMonthDay: 'YYYY-MM-DD',
+  monthDay: 'MMM D',
+  dateTimeMachine: 'YYYY-MM-DDThh:mm',
+  dateTimeHumanize: 'DD/MM/YY hh:mm',
+  time: 'HH:mm',
+};
