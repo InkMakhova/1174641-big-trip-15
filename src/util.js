7a06d8f81beb1e419209388c164f440a626de2ce
Инна <i.m.makhova@gmail.com>
2021-08-03 14:08:12 +0300

    Добавлена универсальная функция форматирования даты и времени, функция render перенесена в утилиты, добавлена функция получения промежутка времени в днях, часах и минутах

--- src/util.js
+++ src/util.js
@@ -1,5 +1,8 @@
 import dayjs from 'dayjs';
 
+const MINUTES_IN_DAY = 1440;
+const MINUTES_IN_HOUR = 60;
+
 export const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -7,14 +10,39 @@ export const getRandomInteger = (a = 0, b = 1) => {
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
-export const formatDatePointList = (date) => dayjs(date).format('YYYY-MM-DD');
+export const formateDateTime = (date, format) => dayjs(date).format(format);
 
-export const humanizeDatePointList = (date) => dayjs(date).format('MMM D');
+export const render = (container, template, place = 'beforeend') => {
+  container.insertAdjacentHTML(place, template);
+};
 
-export const formatDateTimePoint = (date) => dayjs(date).format('YYYY-MM-DDThh:mm');
+export const humanizedTimeDuration = (minutes) => {
+  if (minutes >= 0) {
+    if (minutes < MINUTES_IN_HOUR) {
+      return {
+        minutes: minutes,
+        hours: 0,
+        days: 0,
+      };
+    } else if (minutes >= MINUTES_IN_HOUR && minutes < MINUTES_IN_DAY) {
+      return {
+        minutes: minutes%MINUTES_IN_HOUR,
+        hours: Math.trunc(minutes/MINUTES_IN_HOUR),
+        days: 0,
+      };
+    }
+    const days = Math.trunc(minutes/MINUTES_IN_DAY);
+    const minutesInDays = days*MINUTES_IN_DAY;
+    const restMinutes = minutes%MINUTES_IN_DAY;
+    const minutesNumber = (minutes - minutesInDays)%MINUTES_IN_HOUR;
 
-export const formatTimePoint = (date) => dayjs(date).format('hh:mm');
+    return {
+      minutes: restMinutes < MINUTES_IN_HOUR ? restMinutes : minutesNumber,
+      hours: restMinutes < MINUTES_IN_HOUR ? 0 : Math.trunc(restMinutes/MINUTES_IN_HOUR),
+      days: days,
+    };
 
-export const formatDateTimePointForm = (dateTime) => dayjs(dateTime).format('DD/MM/YY hh:mm');
+  }
+};
 
 export const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
