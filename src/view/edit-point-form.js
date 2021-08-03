66a8f5a7036b7c6e7532dbf9538715bc65dc65b1
Инна <i.m.makhova@gmail.com>
2021-08-03 15:45:40 +0300

    Отрисовка блока редактирования с учетом сгенерированных данных

--- src/view/edit-point-form.js
+++ src/view/edit-point-form.js
@@ -1,11 +1,58 @@
-export const createEditPointTemplate = () => (
+import {
+  capitalizeFirstLetter,
+  formateDateTime,
+  getKeyByValue
+} from '../util.js';
+import {
+  formatsDateTime,
+  offerNames
+} from '../constants.js';
+
+const createOffersSection = (dataPoint) => {
+  if (dataPoint.offer && dataPoint.offer.length > 0) {
+    const offerList = dataPoint.offer
+      .map((offer) => `<div class="event__offer-selector">
+        <input class="event__offer-checkbox  visually-hidden" id="event-offer-${getKeyByValue(offerNames, offer.title)}-1" type="checkbox" name="event-offer-${getKeyByValue(offerNames, offer.title)}" checked>
+        <label class="event__offer-label" for="event-offer-${getKeyByValue(offerNames, offer.title)}-1">
+          <span class="event__offer-title">${offer.title}</span>
+          &plus;&euro;&nbsp;
+          <span class="event__offer-price">${offer.price}</span>
+        </label>
+      </div>`).join('');
+
+    return `<section class="event__section  event__section--offers">
+      <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+
+      <div class="event__available-offers">
+      ${offerList}
+      </div>
+    </section>`;
+  }
+  return '';
+};
+
+const createPhotoTemplate = (dataPoint) => {
+  if (dataPoint.destination.pictures && dataPoint.destination.pictures.length > 0) {
+    const pictureList = dataPoint.destination.pictures
+      .map((picture) => `<img class="event__photo" src="${picture.src}" alt="${picture.description}">`).join('');
+
+    return `<div class="event__photos-container">
+        <div class="event__photos-tape">
+          ${pictureList}
+        </div>
+      </div>`;
+  }
+  return '';
+};
+
+export const createEditPointTemplate = (dataPoint) => (
   `<li class="trip-events__item">
     <form class="event event--edit" action="#" method="post">
       <header class="event__header">
         <div class="event__type-wrapper">
           <label class="event__type  event__type-btn" for="event-type-toggle-1">
             <span class="visually-hidden">Choose event type</span>
-            <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
+            <img class="event__type-icon" width="17" height="17" src="img/icons/${dataPoint.type}.png" alt="Event type icon">
           </label>
           <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
 
@@ -68,9 +115,9 @@ export const createEditPointTemplate = () => (
 
         <div class="event__field-group  event__field-group--destination">
           <label class="event__label  event__type-output" for="event-destination-1">
-            Flight
+            ${capitalizeFirstLetter(dataPoint.type)}
           </label>
-          <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Chamonix" list="destination-list-1">
+          <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${dataPoint.destination.name}" list="destination-list-1">
           <datalist id="destination-list-1">
             <option value="Amsterdam"></option>
             <option value="Geneva"></option>
@@ -80,10 +127,10 @@ export const createEditPointTemplate = () => (
 
         <div class="event__field-group  event__field-group--time">
           <label class="visually-hidden" for="event-start-time-1">From</label>
-          <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="18/03/19 12:25">
+          <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${formateDateTime(dataPoint.dateFrom, formatsDateTime.dateTimeHumanize)}">
           &mdash;
           <label class="visually-hidden" for="event-end-time-1">To</label>
-          <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="18/03/19 13:35">
+          <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${formateDateTime(dataPoint.dateTo, formatsDateTime.dateTimeHumanize)}">
         </div>
 
         <div class="event__field-group  event__field-group--price">
@@ -91,7 +138,7 @@ export const createEditPointTemplate = () => (
             <span class="visually-hidden">Price</span>
             &euro;
           </label>
-          <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="160">
+          <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${dataPoint.basePrice}">
         </div>
 
         <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
@@ -101,60 +148,12 @@ export const createEditPointTemplate = () => (
         </button>
       </header>
       <section class="event__details">
-        <section class="event__section  event__section--offers">
-          <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-
-          <div class="event__available-offers">
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>
-              <label class="event__offer-label" for="event-offer-luggage-1">
-                <span class="event__offer-title">Add luggage</span>
-                &plus;&euro;&nbsp;
-                <span class="event__offer-price">50</span>
-              </label>
-            </div>
-
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort" checked>
-              <label class="event__offer-label" for="event-offer-comfort-1">
-                <span class="event__offer-title">Switch to comfort</span>
-                &plus;&euro;&nbsp;
-                <span class="event__offer-price">80</span>
-              </label>
-            </div>
-
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-meal-1" type="checkbox" name="event-offer-meal">
-              <label class="event__offer-label" for="event-offer-meal-1">
-                <span class="event__offer-title">Add meal</span>
-                &plus;&euro;&nbsp;
-                <span class="event__offer-price">15</span>
-              </label>
-            </div>
-
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-seats-1" type="checkbox" name="event-offer-seats">
-              <label class="event__offer-label" for="event-offer-seats-1">
-                <span class="event__offer-title">Choose seats</span>
-                &plus;&euro;&nbsp;
-                <span class="event__offer-price">5</span>
-              </label>
-            </div>
-
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train">
-              <label class="event__offer-label" for="event-offer-train-1">
-                <span class="event__offer-title">Travel by train</span>
-                &plus;&euro;&nbsp;
-                <span class="event__offer-price">40</span>
-              </label>
-            </div>
-          </div>
-        </section>
+        ${createOffersSection(dataPoint)}
 
         <section class="event__section  event__section--destination">
           <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-          <p class="event__destination-description">Chamonix-Mont-Blanc (usually shortened to Chamonix) is a resort area near the junction of France, Switzerland and Italy. At the base of Mont Blanc, the highest summit in the Alps, it's renowned for its skiing.</p>
+          <p class="event__destination-description">${dataPoint.destination.description}</p>
+          ${createPhotoTemplate(dataPoint)}
         </section>
       </section>
     </form>
