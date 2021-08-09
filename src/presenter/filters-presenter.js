0ea348dde2d28b8e988f2da0d5f07aee96dc495d
Инна <i.m.makhova@gmail.com>
2021-08-09 09:31:36 +0300

    Переименован параметр

--- src/presenter/filters-presenter.js
+++ src/presenter/filters-presenter.js
@@ -1,11 +1,11 @@
 import {NOW} from '../constants.js';
 
-export const getFilteredPoints = (dataPoints, filterName) => {
+export const getFilteredPoints = (pointsList, filterName) => {
   const filteredPoints = {
     everything: (points) => points,
     past: (points) => points.filter((point) => point.dateTo < NOW),
     future: (points) => points.filter((point) => point.dateFrom >= NOW),
   };
 
-  return filteredPoints[filterName](dataPoints);
+  return filteredPoints[filterName](pointsList);
 };
