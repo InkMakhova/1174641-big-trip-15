c85d94b96ab51cfb445d8b022b42186a9cab2188
Инна <i.m.makhova@gmail.com>
2021-08-30 14:51:10 +0300

    Удален неиспользуемый код

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import {FilterType} from './constants.js';
 import {getRandomInteger} from './utils/common.js';
 import {
   render,
@@ -7,13 +6,11 @@ import {
 import TripInfoView from './view/trip-info.js';
 import SiteMenuView from './view/site-menu.js';
 import PriceView from './view/price.js';
-//import FiltersView from './view/filter';
 import TripPresenter from './presenter/trip.js';
 import FilterPresenter from './presenter/filter.js';
 import PointsModel from './model/points.js';
 import FilterModel from './model/filter.js';
 import {generateDataPoint} from './mock/point-mock.js';
-import dayjs from 'dayjs';
 
 const POINTS_NUMBER = 20;
 
@@ -34,9 +31,6 @@ render(tripMainElement, tripInfoComponent, RenderPosition.AFTERBEGIN);
 render(tripInfoComponent, new PriceView(getRandomInteger(200, 1000)));
 
 render(siteMenuElement, new SiteMenuView());
-
-// const filtersComponent = new FiltersView(filters, 'everything');
-// render(filterElement, filtersComponent);
 
 const filterPresenter = new FilterPresenter(filterElement, filterModel, pointsModel);
 filterPresenter.init();
