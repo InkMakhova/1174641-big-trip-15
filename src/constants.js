edf618f2b40a211e1165f760211725c265f208c0
Инна <i.m.makhova@gmail.com>
2021-08-30 09:10:54 +0300

    Добавлены методы добавления, удаления и обновления точки

--- src/constants.js
+++ src/constants.js
@@ -221,4 +221,16 @@ export const generateOffer = () => {
   };
 };
 
+export const UserAction = {
+  UPDATE_POINT: 'UPDATE_POINT',
+  ADD_POINT: 'ADD_POINT',
+  DELETE_POINT: 'DELETE_POINT',
+};
+
+export const UpdateType = {
+  PATCH: 'PATCH',
+  MINOR: 'MINOR',
+  MAJOR: 'MAJOR',
+};
+
 
