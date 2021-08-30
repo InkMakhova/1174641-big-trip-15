c83b9e6f2fac85b7a5ef3d889b998cea75b6f7d2
Инна <i.m.makhova@gmail.com>
2021-08-30 13:32:31 +0300

    В main.js рендеринг фильтров с помощью презентора (7.1.9)

--- src/main.js
+++ src/main.js
@@ -7,8 +7,9 @@ import {
 import TripInfoView from './view/trip-info.js';
 import SiteMenuView from './view/site-menu.js';
 import PriceView from './view/price.js';
-import FiltersView from './view/filters';
+//import FiltersView from './view/filter';
 import TripPresenter from './presenter/trip.js';
+import FilterPresenter from './presenter/filter.js';
 import PointsModel from './model/points.js';
 import FilterModel from './model/filter.js';
 import {generateDataPoint} from './mock/point-mock.js';
@@ -17,20 +18,23 @@ const POINTS_NUMBER = 20;
 
 const points = Array.from({length: POINTS_NUMBER}, () => generateDataPoint());
 
-const filters = [
-  {
-    type: 'everything',
-    name: 'EVERYTHING',
-  },
-  {
-    type: 'past',
-    name: 'PAST',
-  },
-  {
-    type: 'future',
-    name: 'FUTURE',
-  },
-];
+// const filters = [
+//   {
+//     type: 'everything',
+//     name: 'EVERYTHING',
+//     count: 0,
+//   },
+//   {
+//     type: 'past',
+//     name: 'PAST',
+//     count: 0,
+//   },
+//   {
+//     type: 'future',
+//     name: 'FUTURE',
+//     count: 0,
+//   },
+// ];
 
 const pointsModel = new PointsModel();
 pointsModel.setPoints(points);
@@ -48,8 +52,11 @@ render(tripInfoComponent, new PriceView(getRandomInteger(200, 1000)));
 
 render(siteMenuElement, new SiteMenuView());
 
-const filtersComponent = new FiltersView(filters, 'everything');
-render(filterElement, filtersComponent);
+// const filtersComponent = new FiltersView(filters, 'everything');
+// render(filterElement, filtersComponent);
+
+const filterPresenter = new FilterPresenter(filterElement, filterModel, pointsModel);
+filterPresenter.init();
 
 const tripContainerElement = document.querySelector('.page-main').querySelector('.trip-events');
 
