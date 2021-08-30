cf7a8a964540244dc89e6418ad0a2557511b9dec
Инна <i.m.makhova@gmail.com>
2021-08-30 12:21:08 +0300

    Изменен механизм подписки на изменения фильтров (7.1.8)

--- src/view/filters.js
+++ src/view/filters.js
@@ -1,48 +1,57 @@
 import {capitalizeFirstLetter} from '../utils/common.js';
 import AbstractView from './abstract.js';
 
-const createFilterItemTemplate = (filter, isChecked) => (
-  `<div class="trip-filters__filter">
+const createFilterItemTemplate = (filter, currentFilterType) => {
+  const {type, name} = filter;
+
+  return `<div class="trip-filters__filter">
     <input
-        id="filter-${filter}"
+        id="filter-${name}"
         class="trip-filters__filter-input visually-hidden"
         type="radio"
         name="trip-filter"
-        value="${filter}"
-        ${isChecked === true ? 'checked' : ''}>
+        value="${type}"
+        ${type === currentFilterType ? 'checked' : ''}>
     <label
       class="trip-filters__filter-label"
-      for="filter-${filter}">${capitalizeFirstLetter(filter)}</label>
-  </div>`);
+      for="filter-${name}">${capitalizeFirstLetter(name)}</label>
+  </div>`;
+};
 
-const createFiltersTemplate = (filterItems) => {
-  const filterItemsElement = Object.keys(filterItems)
-    .map((el) => createFilterItemTemplate(el, filterItems[el])).join('');
+const createFiltersTemplate = (filterItems, currentFilterType) => {
+  const filterItemsTemplate = filterItems
+    .map((filter) => createFilterItemTemplate(filter, currentFilterType))
+    .join('');
+  // const filterItemsElement = Object.keys(filterItems)
+  //   .map((el) => createFilterItemTemplate(el, filterItems[el])).join('');
 
   return `<form class="trip-filters" action="#" method="get">
-      ${filterItemsElement}
+      ${filterItemsTemplate}
       <button class="visually-hidden" type="submit">Accept filter</button>
     </form>`;
 };
 
 export default class Filters extends AbstractView {
-  constructor (filters) {
+  constructor (filters, currentFilterType) {
     super();
 
     this._filters = filters;
-    this._filterChangeHandler = this._filterChangeHandler.bind(this);
+    this._currentFilter = currentFilterType;
+
+    this._filterTypeChangeHandler = this._filterTypeChangeHandler.bind(this);
   }
 
   getTemplate() {
-    return createFiltersTemplate(this._filters);
+    return createFiltersTemplate(this._filters, this._currentFilter);
   }
 
-  _filterChangeHandler(evt) {
-    this._callback.filterChange(evt);
+  _filterTypeChangeHandler(evt) {
+    evt.preventDefault();
+    this._callback.filterTypeChange(evt.target.value);
   }
 
-  setFilterChangeHandler(callback) {
-    this._callback.filterChange = callback;
-    this.getElement().addEventListener('change', this._filterChangeHandler);
+  setFilterTypeChangeHandler(callback) {
+    this._callback.filterTypeChange = callback;
+    this.getElement().addEventListener('change', this._filterTypeChangeHandler);
   }
 }
