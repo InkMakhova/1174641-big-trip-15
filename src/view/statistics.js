82f19f5e4baff55a609d30a57342200c81c1da8e
Инна <i.m.makhova@gmail.com>
2021-09-06 12:10:07 +0300

    Удален вывод в консоль

--- src/view/statistics.js
+++ src/view/statistics.js
@@ -22,7 +22,6 @@ const renderMoneyChart = (moneyCtx, points) => {
   const uniqTypesUpperCase = uniqTypes.map((type) => type.toUpperCase());
 
   const pointByTypeCounts = uniqTypes.map((type) => countCostsByType(points, type));
-  console.log(pointByTypeCounts);
 
   const hexTypes = uniqTypes.map((type) => typeToHex[type]);
 
