477a7c144e02e7279e8ffbe7dc951b312ff8565a
Инна <i.m.makhova@gmail.com>
2021-09-08 11:27:49 +0300

    Удалены неиспользуемые переменные

--- src/constants.js
+++ src/constants.js
@@ -47,131 +47,10 @@ export const PointTypes = {
   SHIP: 'ship',
   DRIVE: 'drive',
   RESTAURANT: 'restaurant',
+  TRANSPORT: 'transport',
 };
 
 export const POINT_TYPES = Object.values(PointTypes);
-
-export const offerOptions = [
-  {
-    title: 'Choose the radio station',
-    price: 30,
-  },
-  {
-    title: 'Choose temperature',
-    price: 170,
-  },
-  {
-    title: 'Drive quickly, I\'m in a hurry',
-    price: 100,
-  },
-  {
-    title: 'Infotainment system',
-    price: 50,
-  },
-  {
-    title: 'Order meal',
-    price: 100,
-  },
-  {
-    title: 'Choose seats',
-    price: 190,
-  },
-  {
-    title: 'Choose meal',
-    price: 120,
-  },
-  {
-    title: 'Upgrade to comfort class',
-    price: 120,
-  },
-  {
-    title: 'Upgrade to business class',
-    price: 120,
-  },
-  {
-    title: 'Add luggage',
-    price: 170,
-  },
-];
-
-export const OffersSetByTypes = {
-  'taxi': [
-    {
-      title: 'Drive quickly, I\'m in a hurry',
-      price: 100,
-    },
-    {
-      title: 'Upgrade to comfort class',
-      price: 120,
-    },
-    {
-      title: 'Choose the radio station',
-      price: 30,
-    },
-  ],
-  'bus': [
-    {
-      title: 'Infotainment system',
-      price: 50,
-    },
-    {
-      title: 'Choose seats',
-      price: 190,
-    },
-  ],
-  'train': [
-    {
-      title: 'Choose meal',
-      price: 120,
-    },
-    {
-      title: 'Upgrade to business class',
-      price: 120,
-    },
-  ],
-  'flight': [
-    {
-      title: 'Choose seats',
-      price: 190,
-    },
-    {
-      title: 'Upgrade to business class',
-      price: 120,
-    },
-    {
-      title: 'Add luggage',
-      price: 170,
-    },
-  ],
-  'check-in': [
-    {
-      title: 'Choose meal',
-      price: 120,
-    },
-  ],
-  'sightseeing': [],
-  'ship': [
-    {
-      title: 'Choose seats',
-      price: 190,
-    },
-    {
-      title: 'Upgrade to business class',
-      price: 120,
-    },
-  ],
-  'drive': [
-    {
-      title: 'Upgrade to comfort class',
-      price: 120,
-    },
-    {
-      title: 'Choose the radio station',
-      price: 30,
-    },
-  ],
-  'restaurant': [],
-};
 
 export const DEFAULT_TYPE = 'flight';
 
@@ -200,34 +79,6 @@ export const FormatsDateTime = {
   HH_MM: 'HH:mm',
   ISO: 'YYYY-MM-DDTHH:mm:ss.000Z',
 
-};
-
-export const offersNames = {
-  upgradeComfort: 'Upgrade to comfort class',
-  chooseComfort: 'Choose comfort class',
-  chooseBusiness: 'Choose business class',
-  upgradeBusiness: 'Upgrade to a business class',
-  airBusiness: 'Upgrade to business class',
-  radio: 'Choose the radio station',
-  temperature: 'Choose temperature',
-  hurry: 'Drive quickly, I\'m in a hurry',
-  slowly: 'Drive slowly',
-  info: 'Infotainment system',
-  orderMeal: 'Order meal',
-  chooseMeal: 'Choose meal',
-  restaurantMeal: 'Order a meal from the restaurant',
-  seats: 'Choose seats',
-  taxi: 'Book a taxi at the arrival point',
-  orderBreakfast: 'Order a breakfast',
-  addBreakfast: 'Add breakfast',
-  wakeup: 'Wake up at a certain time',
-  luggage: 'Add luggage',
-  lounge: 'Business lounge',
-  checkin: 'Choose the time of check-in',
-  checkout: 'Choose the time of check-out',
-  laundry: 'Laundry',
-  music: 'Choose live music',
-  vip: 'Choose VIP area',
 };
 
 export const UserAction = {
