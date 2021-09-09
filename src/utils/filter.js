0aa2c1d1493003891775bd9bef916d1abc41ddff
Инна <i.m.makhova@gmail.com>
2021-09-09 13:34:33 +0300

    Исправлена функция сортировки

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,13 +1,15 @@
+import dayjs from 'dayjs';
 import {FilterType, today} from '../constants.js';
 
-const isActivePoint = (point) => point.dateFrom < today && point.dateTo > today;
+const isActivePoint = (point) =>
+  dayjs(point.dateFrom).isBefore(today, 'D') && !dayjs(point.dateTo).isBefore(today, 'D');
 
 export const filter = {
   [FilterType.EVERYTHING]: (points) => points.slice(),
   [FilterType.PAST]: (points) =>
     points.filter((point) =>
-      point.dateTo < today || isActivePoint(point)),
+      dayjs(point.dateTo).isBefore(today, 'D') || isActivePoint(point)),
   [FilterType.FUTURE]: (points) =>
     points.filter((point) =>
-      point.dateFrom >= today || isActivePoint(point)),
+      !dayjs(point.dateFrom).isBefore(today, 'D') || isActivePoint(point)),
 };
