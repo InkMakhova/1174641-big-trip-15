083933d75c29168fd3fb418251aa2dc4f32d7292
Инна <i.m.makhova@gmail.com>
2021-09-13 17:23:15 +0300

    Удален неиспользуемый файл

--- src/presenter/sort.js
+++
@@ -1,9 +0,0 @@
-export const getSortedPoints = (pointsList, sortName) => {
-  const sortedPoints = {
-    'sort-day': (points) => points.slice().sort((a, b) => a.dateFrom - b.dateFrom),
-    'sort-price': (points) => points.slice().sort((a, b) => b.basePrice - a.basePrice),
-    'sort-time': (points) => points.slice().sort((a, b) => b.duration - a.duration),
-  };
-
-  return sortedPoints[sortName](pointsList);
-};
