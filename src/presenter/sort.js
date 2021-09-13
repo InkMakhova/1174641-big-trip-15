c162203fbede984a6be912bab99446e3665d7dd8
Инна <i.m.makhova@gmail.com>
2021-09-13 17:07:58 +0300

    Сортировка исправлена от старых к новым точкам

--- src/presenter/sort.js
+++ src/presenter/sort.js
@@ -1,6 +1,6 @@
 export const getSortedPoints = (pointsList, sortName) => {
   const sortedPoints = {
-    'sort-day': (points) => points.slice().sort((a, b) => b.dateFrom - a.dateFrom),
+    'sort-day': (points) => points.slice().sort((a, b) => a.dateFrom - b.dateFrom),
     'sort-price': (points) => points.slice().sort((a, b) => b.basePrice - a.basePrice),
     'sort-time': (points) => points.slice().sort((a, b) => b.duration - a.duration),
   };
