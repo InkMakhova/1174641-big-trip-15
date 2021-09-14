a5292fef2d5f4887c42888249bc5bc63f1c8f86f
Инна <i.m.makhova@gmail.com>
2021-09-14 23:26:39 +0300

    Удален неиспользуемый код

--- src/utils/common.js
+++ src/utils/common.js
@@ -2,13 +2,6 @@ import {humanizeTimeDuration} from '../utils/point.js';
 
 export const isOnline = () => window.navigator.onLine;
 
-export const getRandomInteger = (a = 0, b = 1) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-
-  return Math.floor(lower + Math.random() * (upper - lower + 1));
-};
-
 export const capitalizeFirstLetter = (string) =>
   string.charAt(0).toUpperCase() + string.slice(1);
 
